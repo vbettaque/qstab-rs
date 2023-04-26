a62e2f1e85c1f04d1a70ff955f911be7dca2ddc6
Valerie Bettaque <v.l.bettaque@gmail.com>
2023-04-26 19:18:13 -0400

    began work on o(2n) sampling

--- src/orthogonal.rs
+++ src/orthogonal.rs
@@ -1,7 +1,11 @@
-use nalgebra::SquareMatrix;
+use std::ops::BitOr;
 
-pub fn even_group_order(n: u32) -> usize {
-    assert_eq!(n % 2, 0);
+use nalgebra::{DimName, allocator::Allocator, DefaultAllocator, OMatrix};
+
+use crate::fields::GF2;
+
+fn group_order(n: u32) -> usize {
+    assert!(n % 2 == 0);
     
     let k: u32 = n / 2;
     let base: usize = 2;
@@ -13,4 +17,28 @@ pub fn even_group_order(n: u32) -> usize {
     order
 }
 
-// pub fn get_orthogonal_matrix() -> SquareMatrix<GF2, Dyn, S>
+pub fn get_orthogonal_matrix<D: DimName>(i: usize) -> OMatrix::<GF2, D, D>
+where 
+    DefaultAllocator: Allocator<GF2, D, D>
+{
+    let n = D::dim() as u32;
+    let ord = group_order(n);
+    let base: usize = 2;
+
+    assert!(i < ord);
+
+    let mut o: OMatrix<GF2, D, D> = OMatrix::<GF2, D, D>::zeros();
+
+    let s: usize = base.pow(n);
+    let mut k = i % s;
+    k = if k.count_ones() % 2 == 0 {k | s} else {k};
+
+    let v = (0..n).map(|n| (k >> n) & 1);
+
+
+    
+    let b1 = o.column_mut(0);
+    let b2 = o.column_mut(1);
+    
+    return o
+}
